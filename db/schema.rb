c12d411ffc1c5197e10fae5bfc5d40132dcc391b
Tyler Hartland <tylerhartland7@gmail.com>
2016-03-23 19:18:14 -0500

    basic Repository model

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20160324001234) do
+
+  create_table "repositories", force: :cascade do |t|
+    t.string "url"
+  end
 
 end
---
+++ spec/models/repository_spec.rb
@@ -0,0 +1,2 @@
+RSpec.describe Repository do
+end

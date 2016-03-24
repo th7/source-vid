97d68e49dd21f71dfa919e922081f347f538e389
Tyler Hartland <tylerhartland7@gmail.com>
2016-03-24 08:42:49 -0500

    Repository can generate a reasonable filename

--- app/models/repository.rb
+++ app/models/repository.rb
@@ -1,2 +1,6 @@
 class Repository < ActiveRecord::Base
+  def generate_filename
+    uri = URI(url)
+    (uri.host + uri.path).gsub('/', '-')
+  end
 end
--- spec/models/repository_spec.rb
+++ spec/models/repository_spec.rb
@@ -1,2 +1,9 @@
 RSpec.describe Repository do
+  let(:repository) { Repository.new(url: 'https://github.com/th7/journey-tracker') }
+
+  describe '#generate_filename' do
+    it 'generates a file path from the url' do
+      expect(repository.generate_filename).to eq 'github.com-th7-journey-tracker'
+    end
+  end
 end

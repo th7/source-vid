53386fdf50dc70b56502f368c67a56db23a05469
Tyler Hartland <tylerhartland7@gmail.com>
2016-03-24 08:32:47 -0500

    video adapter is mocked

--- app/adapters/video_adapter.rb
+++ app/adapters/video_adapter.rb
@@ -1,10 +1,37 @@
+require 'open3'
+
 class VideoAdapter
   def initialize(path_to_repo)
     @path_to_repo = path_to_repo
   end
 
+  # mocked for now
+  # the command below doesn't seem to terminate
+  # and stopping it programmatically doesn't seem to work
   def write_to(output_path)
-    cmd = "cd #{@path_to_repo} && gource -1280x720 -o - | ffmpeg -y -r 60 -f image2pipe -vcodec ppm -i - -vcodec libx264 -preset ultrafast -pix_fmt yuv420p -crf 1 -threads 0 -bf 0 #{output_path}"
-    raise 'could not create video' unless system(cmd)
+    FileUtils.cp('/tmp/gource-mocked.mp4', output_path)
+  end
+
+  private
+
+  def actual_write_to(output_path, timeout=3)
+    cmd = "gource -640x480 -o - #{@path_to_repo} | ffmpeg -y -r 60 -f image2pipe -vcodec ppm -i - -vcodec libx264 -preset ultrafast -pix_fmt yuv420p -crf 1 -threads 0 -bf 0 #{output_path}"
+    Open3.popen3(cmd) do |stdin, stdout, stderr, wait_thread|
+      set_timeout(wait_thread.pid, timeout)
+      stdin.close
+      wait_thread.join
+    end
+  end
+  # it seems possible to interrupt gource / ffmpeg gracefully using ctrl+c on a foreground process
+  # this creates a working if shorter video
+  # this method was intended to programmatically do the same thing
+  # but the process doesn't seem to respond
+  # and using exa. QUIT seems to produce a corrupt video file
+  def set_timeout(pid, timeout)
+    # pid = wait_thread.pid
+    # Thread.new do
+    #   sleep timeout
+    #   Process.kill('INT', pid)
+    # end
   end
 end
--- spec/adapters/video_adapter_spec.rb
+++ spec/adapters/video_adapter_spec.rb
@@ -2,9 +2,10 @@ RSpec.describe VideoAdapter do
   describe('#write_to') do
     let(:adapter) { VideoAdapter.new('/fake/path/to/repo') }
 
-    it 'calls the expected system command' do
-      expect(adapter).to receive(:system).with('cd /fake/path/to/repo && gource -1280x720 -o - | ffmpeg -y -r 60 -f image2pipe -vcodec ppm -i - -vcodec libx264 -preset ultrafast -pix_fmt yuv420p -crf 1 -threads 0 -bf 0 /fake/path/to/file.mp4').and_return(true)
-      adapter.write_to('/fake/path/to/file.mp4')
-    end
+    pending 'implementing video generation is currently mocked'
+    # it 'calls the expected system command' do
+    #   # expect(adapter).to receive(:system).with('cd /fake/path/to/repo && gource -320x240 -o - | ffmpeg -y -r 60 -f image2pipe -vcodec ppm -i - -vcodec libx264 -preset ultrafast -pix_fmt yuv420p -crf 1 -threads 0 -bf 0 /fake/path/to/file.mp4').and_return(true)
+    #   # adapter.write_to('/fake/path/to/file.mp4')
+    # end
   end
 end

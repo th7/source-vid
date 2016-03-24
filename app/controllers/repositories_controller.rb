270d7c4a9493160a237a48f6d75a7160152f571f
Tyler Hartland <tylerhartland7@gmail.com>
2016-03-24 09:39:03 -0500

    can trigger processing via web ui

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -15,4 +15,17 @@ class RepositoriesController < ApplicationController
       type: "video/mp4"
     )
   end
+
+  def start
+    Repository.transaction do
+      repo = Repository.lock.find(params[:repository_id])
+      if repo.inactive?
+        VideoWorker.perform_async(repo.id)
+        repo.queued!
+      else
+        raise 'make me a friendlier error noob!'
+      end
+    end
+    redirect_to repository_path(repo.id)
+  end
 end

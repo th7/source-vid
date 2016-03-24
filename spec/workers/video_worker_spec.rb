65bc7dbf7dfe4b03476cb59c0b705e3e4e868297
Tyler Hartland <tylerhartland7@gmail.com>
2016-03-24 08:54:35 -0500

    VideoWorker saves video files and stores path to file

--- spec/workers/video_worker_spec.rb
+++ spec/workers/video_worker_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe VideoWorker do
       FileUtils.rm_rf('/tmp/gource.mp4')
       repo = Repository.create(url: 'https://github.com/th7/journey-tracker')
       VideoWorker.new.perform(repo.id)
-      expect(File.exists?('/tmp/gource.mp4')).to eq true
+      expect(File.exists?(repo.reload.video_filepath)).to eq true
     end
   end
 end
